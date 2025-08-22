eaa25e12fdadc04a1c44cac2124a89473bac2c5d
Mohammadreza Naseri <themohammadreza2006@gmail.com>
2025-08-22 23:52:22 +0330

    Create Project Structure

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import random
+
+def monty_hall_game(switch: bool) -> bool:
+    pass
+
+def game_simulation(num_of_reapet: int, switch: bool) -> float:
+    pass
+
+if __name__ == "__main__":
+    pass
